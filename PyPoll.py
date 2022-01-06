8e10b05ba33bc5b3988d784d4f2ef9c0b9768f02
kflores4716 <kflores4716@gmail.com>
2022-01-06 16:25:22 -0700

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -11,22 +11,22 @@
 
 # print("The time right now is ", now)
 
-from typing import ClassVar
+# from typing import ClassVar
 
 
 
 
 #Assign a variable for the file to load and the path.
-file_to_load = 'Resources\election_results.csv'
+# file_to_load = 'Resources\election_results.csv'
 
 # Open the election results and read the file
-with open(file_to_load) as election_data:
+# with open(file_to_load) as election_data:
 
     #To do: perform analysis.
-    print(election_data)
+    # print(election_data)
 
 #Close the file.
-election_data.close()
+# election_data.close()
 
 #Add our dependencies
 import csv
@@ -38,6 +38,19 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# 1. Initialize a total vote counter.
+total_votes = 0
+# Declaring a list of candidate options
+candidate_options = []
+
+# Declare candidate votes dictionary
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -45,9 +58,66 @@ with open(file_to_load) as election_data:
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    # Print each row in the CSV file.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+    
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidate's vote count
+            candidate_votes[candidate_name] = 0
+
+        # Tracking candidate vote counts
+        candidate_votes[candidate_name] += 1
+
+        # Determine the percentage of votes for each candidate by looping through the counts.
+        # 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    print(f'{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n')
+
+    # Determine winning vote count and candidate
+    # Determine if the votes is greater than the winning count
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true then set winning_count = votes and winning_percent = voting_percentage
+        # vote_percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # Set winning_candidate equal to the candidate's name
+        winning_candidate = candidate_name
+
+winning_candidate_summary = (
+    f'--------------------------\n'
+    f'Winner: {winning_candidate}\n'
+    f'Winning Vote Count: {winning_count:,}\n'
+    f'Winning Percentage: {winning_percentage:.1f}%\n'
+    f'---------------------------\n')
+
+print(winning_candidate_summary)
+
+# 3. Print the total votes.
+print(total_votes)
+
+# Print the candidate list
+print(candidate_options)
+
+# Print the candidate vote dictionary.
+print(candidate_votes)
+
 
 
 #Using the open() function with the "w" mode we will write data to the file
@@ -58,8 +128,5 @@ with open(file_to_load) as election_data:
     # txt_file.write("Denver, ")
     # txt_file.write("Jefferson") SEE BELOW FOR BETTER METHOD
     # txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
-
-
-
 
 
